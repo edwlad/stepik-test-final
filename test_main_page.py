c56873a55635cd0d768942c8a92536d1ac579b92
edwlad <80802093+edwlad@users.noreply.github.com>
2021-06-09 14:02:31 +0700

    Correct test_main_page.py

--- test_main_page.py
+++ test_main_page.py
@@ -1,18 +1,15 @@
 # pytest -s -v --tb=line --language=en test_main_page.py
 
 import pytest
+from pages.main_page import MainPage
 
 links = [
     'http://selenium1py.pythonanywhere.com/',
 ]
 
 
-def go_to_login_page(browser):
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
-
-
 @pytest.mark.parametrize('link', links)
 def test_guest_can_go_to_login_page(browser, link):
-    browser.get(link)
-    go_to_login_page(browser)
+    page = MainPage(browser, link)  # инициализируем Page Object
+    page.open()  # открываем страницу
+    page.go_to_login_page()  # выполняем метод страницы
