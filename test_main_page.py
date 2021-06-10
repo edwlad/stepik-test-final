ea3abe49d2d98c80a586f33f22dabb48c2bdd222
edwlad <80802093+edwlad@users.noreply.github.com>
2021-06-10 11:52:17 +0700

    Add login_page.py

--- test_main_page.py
+++ test_main_page.py
@@ -2,15 +2,17 @@
 
 import pytest
 from pages.main_page import MainPage
+# from pages.login_page import LoginPage
 
 links = [
     'http://selenium1py.pythonanywhere.com/',
+    'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer',
 ]
 
 
 @pytest.mark.parametrize('link', links)
 def test_guest_can_go_to_login_page(browser, link):
-    page = MainPage(browser, link)  # инициализируем Page Object
+    page = MainPage(browser, link, 1)  # инициализируем Page Object
     page.open()  # открываем страницу
     page.should_be_login_link()
     page.go_to_login_page()  # выполняем метод страницы
